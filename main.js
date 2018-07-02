117255259f2e134ede9048147727f6bbfd6d88e1
SoNiceElijah <Superil2q@yandex.ru>
2018-07-02 12:33:51 +0300

    base js info

---
+++ main.js
@@ -0,0 +1,34 @@
+var name = 'jknfjksndjk gsndkjngsd g';
+var name1 = 2551;
+var name2 = 325.235236236;
+var name3 = true;
+var name4 = '\t';
+
+var a = 14;
+var b = 12.6;
+
+var c = a + b;
+
+var d = name3 + name3;
+
+var f = (a==b);
+
+var s = [1,2,35,5,547,568,7,5,3];
+
+for(var i = 0; i < s.length; i++)  // цикл
+{
+    if(s[i]== 35)   // нашли 35 из массива данных 
+        console.log(i);  // условие, чтобы вывести номер элемента 
+}
+
+var j = 0;
+while(name[j]!=' ')  // бесконечный цикл, пишем его, если уверены, что условие выполнется 
+{
+    j++; // прибавляет значение к j
+}
+
+console.log(j);
+
+
+
+console.log(f);
