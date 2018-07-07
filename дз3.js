b8e2ca0d7c698a0ae9942227e81a36d016da04fd
SoNiceElijah <Superil2q@yandex.ru>
2018-07-07 15:34:07 +0300

    Новое в дз3

--- дз3.js
+++ дз3.js
@@ -1,5 +1,19 @@
-var fact = 1
+var fact = 1;
+
 for (var j = 6; j > 0; j--)
 {
     fact = fact * j;
 }
+
+var res = fuct(6);
+
+function fuct(a)
+{
+    if(a == 1) 
+        return 1;
+    else
+        return a * fuct(a-1);
+}
+
+
+console.log(res);
